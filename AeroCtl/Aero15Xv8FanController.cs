40925d9d1185ffdd00751107ed9045aa3f808ba7
Cobra Fast <cobra_fast@wtwrp.de>
2019-08-06 18:55:23 +0200

    Refactor and fix Aero 15Xv8 fan controller. Curve control still not implemented.

--- AeroCtl/Aero15Xv8FanController.cs
+++ AeroCtl/Aero15Xv8FanController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Management;
 using System.Threading.Tasks;
 
 namespace AeroCtl
@@ -13,105 +14,127 @@ namespace AeroCtl
 			this.wmi = wmi;
 		}
 
-		public bool AutoFanStatus
+		public async Task<bool> GetAutoFanStatus()
 		{
-			get => this.wmi.InvokeGet<byte>("GetAutoFanStatus") != 0;
-			set => this.wmi.InvokeSet<byte>("SetAutoFanStatus", value ? (byte) 1 : (byte) 0);
+			return await this.wmi.InvokeGetAsync<byte>("GetAutoFanStatus") != 0;
 		}
 
-		public bool FanFixedStatus
+		public async Task SetAutoFanStatus(bool value)
 		{
-			get => this.wmi.InvokeGet<byte>("GetFanFixedStatus") != 0;
-			set => this.wmi.InvokeSet<byte>("SetFixedFanStatus", value ? (byte)1 : (byte)0);
+			await this.wmi.InvokeSetAsync<byte>("SetAutoFanStatus", value ? (byte)1 : (byte)0);
 		}
 
-		public byte FixedFanSpeed
+		public async Task<bool> GetFanFixedStatus()
 		{
-			set => this.wmi.InvokeSet<byte>("SetFixedFanSpeed", value);
+			return await this.wmi.InvokeGetAsync<byte>("GetFanFixedStatus") != 0;
 		}
 
-		public bool FanSpeed
+		public async Task SetFanFixedStatus(bool value)
 		{
-			get => this.wmi.InvokeGet<byte>("GetFanSpeed") != 0;
-			set => this.wmi.InvokeSet<byte>("SetFanSpeed", value ? (byte) 1 : (byte) 0);
+			await this.wmi.InvokeSetAsync<byte>("SetFixedFanStatus", value ? (byte)1 : (byte)0);
 		}
 
-		public int CurrentFanStep
+		public async Task SetFixedFanSpeed(byte value)
 		{
-			set => this.wmi.InvokeSet<int>("SetCurrentFanStep", value);
+			await this.wmi.InvokeSetAsync<byte>("SetFixedFanSpeed", value);
 		}
 
-		public bool StepFanStatus
+		public async Task<bool> GetFanSpeed()
 		{
-			get => this.wmi.InvokeGet<byte>("GetStepFanStatus") != 0;
-			set => this.wmi.InvokeSet<byte>("SetStepFanStatus", value ? (byte) 1 : (byte) 0);
+			return await this.wmi.InvokeGetAsync<byte>("GetFanSpeed") != 0;
 		}
 
-		public bool SmartCoolingStatus
+		public async Task SetFanSpeed(bool value)
 		{
-			get => this.wmi.InvokeGet<byte>("GetSmartCool") != 0;
-			set => this.wmi.InvokeSet<byte>("SetSmartCool", value ? (byte) 1 : (byte) 0);
+			try
+			{
+				await this.wmi.InvokeSetAsync<byte>("SetFanSpeed", value ? (byte) 1 : (byte) 0);
+			}
+			catch (ManagementException) // Always thrown by design.
+			{ }
+		}
+
+		public async Task SetCurrentFanStep(byte value)
+		{
+			await this.wmi.InvokeSetAsync<byte>("SetCurrentFanStep", value);
+		}
+
+		public async Task<bool> GetStepFanStatus()
+		{
+			return await this.wmi.InvokeGetAsync<byte>("GetStepFanStatus") != 0;
+		}
+
+		public async Task SetStepFanStatus(bool value)
+		{
+			await this.wmi.InvokeSetAsync<byte>("SetStepFanStatus", value ? (byte)1 : (byte)0);
+		}
+
+		public async Task<bool> GetSmartCoolingStatus()
+		{
+			return await this.wmi.InvokeGetAsync<byte>("GetSmartCool") != 0;
+		}
+
+		public async Task SetSmartCoolingStatus(bool value)
+		{
+			try
+			{
+				await this.wmi.InvokeSetAsync<byte>("SetSmartCool", value ? (byte) 1 : (byte) 0);
+			}
+			catch (ManagementException) // Always thrown by design.
+			{ }
 		}
 
 		#region IFanController
 
 		public async Task<(int fan1, int fan2)> GetRpmAsync()
 		{
-			int rpm1 = await this.wmi.InvokeGetAsync<ushort>("getRpm1");
-			int rpm2 = await this.wmi.InvokeGetAsync<ushort>("getRpm2");
+			int rpm1 = reverse(await this.wmi.InvokeGetAsync<ushort>("getRpm1"));
+			int rpm2 = reverse(await this.wmi.InvokeGetAsync<ushort>("getRpm2"));
 
 			return (rpm1, rpm2);
 		}
 
-		public Task SetQuietAsync()
+		public async Task SetQuietAsync()
 		{
-			FanFixedStatus = false;
-			FanSpeed = false;
-			StepFanStatus = false;
-			CurrentFanStep = 0;
-			AutoFanStatus = false;
-			SmartCoolingStatus = true;
-
-			return Task.CompletedTask;
+			await SetFanFixedStatus(false);
+			await SetFanSpeed(false);
+			await SetStepFanStatus(false);
+			await SetCurrentFanStep(0);
+			await SetAutoFanStatus(false);
+			await SetSmartCoolingStatus(true);
 		}
 
-		public Task SetNormalAsync()
+		public async Task SetNormalAsync()
 		{
-			FanFixedStatus = false;
-			FanSpeed = false;
-			SmartCoolingStatus = false;
-			StepFanStatus = false;
-			CurrentFanStep = 0;
-			AutoFanStatus = false;
-
-			return Task.CompletedTask;
+			await SetFanFixedStatus(false);
+			await SetFanSpeed(false);
+			await SetStepFanStatus(false);
+			await SetCurrentFanStep(0);
+			await SetAutoFanStatus(false);
+			await SetSmartCoolingStatus(false);
 		}
 
-		public Task SetGamingAsync()
+		public async Task SetGamingAsync()
 		{
-			FanFixedStatus = false;
-			FanSpeed = false;
-			SmartCoolingStatus = false;
-			StepFanStatus = false;
-			CurrentFanStep = 0;
-			AutoFanStatus = true;
-
-			return Task.CompletedTask;
+			await SetFanFixedStatus(false);
+			await SetFanSpeed(false);
+			await SetStepFanStatus(false);
+			await SetCurrentFanStep(0);
+			await SetAutoFanStatus(true);
+			await SetSmartCoolingStatus(false);
 		}
 
-		public Task SetFixedAsync(double fanSpeed = 0.25)
+		public async Task SetFixedAsync(double fanSpeed = 0.25)
 		{
 			Debug.Assert(fanSpeed >= 0.0 && fanSpeed <= 1.0);
 
-			FanSpeed = false;
-			SmartCoolingStatus = false;
-			StepFanStatus = false;
-			AutoFanStatus = false;
-			CurrentFanStep = 0;
-			FanFixedStatus = true;
-			FixedFanSpeed = (byte)Math.Round(fanSpeed * 229.0);
-
-			return Task.CompletedTask;
+			await SetFanFixedStatus(true);
+			await SetFanSpeed(false);
+			await SetStepFanStatus(false);
+			await SetCurrentFanStep(0);
+			await SetAutoFanStatus(false);
+			await SetSmartCoolingStatus(true);
+			await SetFixedFanSpeed((byte)Math.Round(fanSpeed * 229.0));
 		}
 
 		public Task SetAutoAsync(double fanAdjust = 0.25)
@@ -130,5 +153,9 @@ namespace AeroCtl
 		}
 
 		#endregion
+		private static ushort reverse(ushort val)
+		{
+			return (ushort)((val << 8) | (val >> 8));
+		}
 	}
 }
